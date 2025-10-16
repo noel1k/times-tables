85de0a0809330181958f74bc5cce34080f92d09b
noel1k <noelrafferty999@outlook.com>
2025-10-16 11:22:14 +0100

    Add files via upload

---
+++ times tables 2.py
@@ -0,0 +1,7 @@
+count = 0
+user= int(input("Please input the number you want the times tables for: "))
+user1 = int(input("Please input the amount of times you want to see: "))
+for x in range(1 ,user1 + 1):
+    count+= 1
+    print(user * count)
+    
